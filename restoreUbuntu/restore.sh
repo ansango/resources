440404d42f6e1886956c559084466e4dfec35781
Aníbal Santos Gómez <anibalsantosgo@gmail.com>
2021-02-01 19:29:51 +0100

    Update restore.sh

--- restoreUbuntu/restore.sh
+++ restoreUbuntu/restore.sh
@@ -8,8 +8,6 @@ sudo apt -y upgrade
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-sudo apt-get install firefox-dev
-
 sudo apt -y install git
 sudo apt -y install exfat-fuse exfat-utils
 
